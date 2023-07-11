a02d8ce808e55336840ea5622b732e9ba794ff90
jaredatandi <jaredatandi07@gmail.com>
2023-07-11 08:27:03 +0300

    Basic concat, strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,3 +3,14 @@
 """
 
 
+def concat(str1: str, str2: str) -> str:
+    """Concat a string
+
+    Args:
+        str1 (str):
+        str2 (str):
+
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
