518140ab309053c600a98e3aa42f1854ed37bd07
jaredatandi <jaredatandi07@gmail.com>
2023-07-11 09:16:26 +0300

    Use complex types -lists of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A complex type annotations
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Complex annotations
+
+    Args:
+        input_list (list[float]): _description_
+
+    Returns:
+        float: _description_
+    """
+    ans = 0
+    for i in range(len(input_list)):
+        ans += input_list[i]
+
+    return ans
