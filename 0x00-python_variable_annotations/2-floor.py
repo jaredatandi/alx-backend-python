fe0b6626e539164e6f02fdd52f2e3f97df7f4445
jaredatandi <jaredatandi07@gmail.com>
2023-07-11 08:39:49 +0300

    Basic annotations -floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A module to return a floor of a number
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Basic annotations
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        int: _description_
+    """
+
+    x = math.floor(n)
+    return x
