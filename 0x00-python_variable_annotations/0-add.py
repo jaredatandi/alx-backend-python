af879970ee7c51a1817f3659b4d4bc72f95e8f53
jaredatandi <jaredatandi07@gmail.com>
2023-07-11 08:15:23 +0300

    Use basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module to return the sum of numbers: float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Addition function
+
+    Args:
+        a (float): first float 
+        b (float): second float 
+
+    Returns:
+        float: sum of the float numbers 
+    """
+    return (a + b)
