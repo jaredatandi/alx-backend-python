b34aba359a601b36d0489f1f41e97e98885c0a06
jaredatandi <jaredatandi07@gmail.com>
2023-07-11 08:58:37 +0300

    Basic annotation - to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor
+"""
+
+
+def to_str(n: float) -> str:
+    """To string convert
+
+    Args:
+      n (float): _description_
+
+    Returns:
+      str: _description_
+    """
+    return str(n)
